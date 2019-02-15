b2fc3c5072b31c9ec77e5d7c14cf1cc04f1e85fe
YerlanTemir <170103024@stu.sdu.edu.kz>
2019-02-15 10:55:11 +0600

    style (scrapper.py function):put authentification process into one function

--- scrapper.py
+++ scrapper.py
@@ -3,29 +3,32 @@ from lxml import html
 import secret_data as sd
 from bs4 import BeautifulSoup
 
-name = sd.get_login()
-password = sd.get_password()
+def get_schedule_html():
 
-payload = {"username": name, "password" : password, "LogIn":" Log in "}
-session_requests = requests.session()
+    name = sd.get_login()
+    password = sd.get_password()
 
-login_url = "https://my.sdu.edu.kz/"
-result = session_requests.get(login_url)
+    payload = {"username": name, "password" : password, "LogIn":" Log in "}
+    session_requests = requests.session()
 
-tree = html.fromstring(result.text)
-auth_token = list(set(tree.xpath("//input[@name='LogIn']/@value")))[0]
+    login_url = "https://my.sdu.edu.kz/"
+    result = session_requests.get(login_url)
 
-result = session_requests.post(
-    login_url,
-    data=payload,
-    headers = dict(referer = login_url)
-)
+    tree = html.fromstring(result.text)
+    auth_token = list(set(tree.xpath("//input[@name='LogIn']/@value")))[0]
 
-url = 'https://my.sdu.edu.kz/index.php?mod=schedule'
-result = session_requests.get(url,headers = dict(referer = url))
+    result = session_requests.post(
+        login_url,
+        data=payload,
+        headers = dict(referer = login_url)
+    )
 
-tree = html.fromstring(result.content)
+    url = 'https://my.sdu.edu.kz/index.php?mod=schedule'
+    result = session_requests.get(url,headers = dict(referer = url))
 
-soup = BeautifulSoup(result.content,'html.parser')
+    tree = html.fromstring(result.content)
 
+    return BeautifulSoup(result.content,'html.parser')
+
+soup = get_schedule_html()
 print(soup)
