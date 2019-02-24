e9e172bc8e7cbfd5e6a8628ff123ba86c8fc852e
YerlanTemir <170103024@stu.sdu.edu.kz>
2019-02-24 17:34:57 +0600

    feat(scrapper.py):put everything into Schedule class

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import logging
 from telegram.ext import CommandHandler,MessageHandler,Filters,ConversationHandler
 import datetime
 import secret_data as sd
+import scrapper
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
@@ -12,30 +13,7 @@ username = ''
 password = ''
 
 USERNAME,PASSWORD,GENDER = range(3)
-weekdays = {1:[datetime.time(15,00,0) ],
-            
-            2:[datetime.time(10,00,0),    
-            datetime.time(11,00,0),   
-            datetime.time(12,00,0),    
-            datetime.time(13,00,0),   
-            datetime.time(14,0,0), 
-            datetime.time(16,0,0) ],
-            
-            3:[datetime.time(9,00,0),    
-            datetime.time(10,00,0),   
-            datetime.time(12,00,0),    
-            datetime.time(13,00,0),   
-            datetime.time(14,00,0) ],
-            
-            4:[datetime.time(10,00,0),    
-            datetime.time(11,00,0),   
-            datetime.time(12,00,0),    
-            datetime.time(14,00,0),   
-            datetime.time(20,00,0) ],
-            
-            5:[datetime.time(13,00,0),    
-            datetime.time(14,00,0),   
-            datetime.time(19,00,0) ]}
+
 
 
 
@@ -45,24 +23,30 @@ def callback_get_time(bot,update):
     time = datetime.time(date.hour,date.minute,date.second)
     weekday = date.isoweekday()
     days = 0
-
-    if(weekday > 5):
-        days += 8 - weekday
-        weekday = 1
     
+    if(weekday >= 6):
+        days = 8-weekday
+        weekday = 1
+
     indexOfMin = closest_index(time,weekday)
+    
     if days > 0:
+
         indexOfMin = 0
-    next_lesson_time = weekdays[weekday][indexOfMin]
+
+    next_lesson = weekdays[weekday][indexOfMin]
+    next_lesson_time = next_lesson.time
     
     next_lesson_date = datetime.datetime(date.year,
     date.month,date.day+days,next_lesson_time.hour,next_lesson_time.minute,00)
 
     bot.send_message(chat_id = update.message.chat_id , 
-            text = 'Until next lesson:{}'.format(next_lesson_date - date))
+            text = 'Your next lesson is {}\nWhere?{}\nTeacher:{} \nuntil lesson:{}'.format(next_lesson.title,next_lesson.room,
+                next_lesson.teacher_name,next_lesson_date - date))
 
     
     
+
 def closest_index(time,weekday):
     my_time_minutes = time.hour*60 + time.minute
     min = 24*60
@@ -71,7 +55,8 @@ def closest_index(time,weekday):
     global weekdays
     for i in range(len(weekdays[weekday])):
         
-        minutes = weekdays[weekday][i].hour*60+weekdays[weekday][i].minute
+        subject_time = weekdays[weekday][i].time
+        minutes = subject_time.hour*60+subject_time.minute
         difference = minutes - my_time_minutes 
 
         if(difference < min and difference > 0):
@@ -83,22 +68,35 @@ def closest_index(time,weekday):
 
 
 
+
 def start(bot,update):
     update.message.reply_text('Enter your login:')
 
     return USERNAME
     
 
+
 def get_username(bot,update):
+    
     global username
     username = update.message.text
     update.message.reply_text('Now,enter your password:')
     
     return PASSWORD
 
+
 def get_password(bot,update):
+    
     global password
     password = update.message.text
+    update.message.reply_text('Wait about 10 seconds,we are getting data from portal')
+
+    schedule = scrapper.Schedule(username,password)
+    global weekdays
+    weekdays = schedule.get_schedule_data()
+
+    update.message.reply_text('Your are welcome,now you can use our bot!')
+    
     
     return 
 
@@ -127,6 +125,7 @@ def main():
 
         fallbacks=[CommandHandler('cancel', cancel)]
     )
+   
 
     dp.add_handler(CommandHandler('next',callback_get_time))
     dp.add_handler(conv_handler)    
--- scrapper.py
+++ scrapper.py
@@ -4,94 +4,106 @@ from time import sleep
 from bs4 import BeautifulSoup
 import datetime
 
-username = sd.get_login()
-password = sd.get_password()
 
-driver = webdriver.Firefox()
 
-driver.get('https://my.sdu.edu.kz/')
 
 class Subject:
 
-    def __init__(self,title,teacher_name,room,weekday,timeAtt):
+    def __init__(self,title,teacher_name,room,timeAtt):
         
         self.title = title
         self.teacher_name = teacher_name
         self.room = room
-        self.weekday = weekday
         self.timeAtt = timeAtt.split(':')
         self.time = datetime.time(int(self.timeAtt[0]),int(self.timeAtt[1]),00)
         
     
 
     def __lt__ (self, other):
+        return self.time < other.time
         
-        if self.weekday == other.weekday:
-            return self.time < other.time
-        return self.weekday < other.weekday
 
     def __eq__ (self, other):
-        return self.weekday == other.weekday and self.time == other.time
+        return self.time == other.time
 
     def __repr__(self):
-        return '{},{},{},{},{}'.format(self.title,self.teacher_name,self.room,self.weekday,self.time)
+        return '{},{},{},{}'.format(self.title,self.teacher_name,self.room,self.time)
 
 
-def login():
-    global driver
-    driver.find_element_by_id("username").send_keys(username)
-    driver.find_element_by_id("password").send_keys(password)
-    driver.find_element_by_class_name("q-button").click()
 
-def get_to_schedule():
-    login()
-    
-    global driver
-    driver.find_element_by_css_selector(".leftLinks a[href^='?mod=schedule'] ").click()
-    sleep(2)
-    html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
-    return html
-    
+class Schedule:
 
-def get_schedule_data():
     
-    html = get_to_schedule()
-    soup = BeautifulSoup(html,'lxml')
-    
-    lists = soup.find('div',id='div_results').find_all('tr')
-    subjects = []
+    def __init__(self,username,password):
 
-    #i = time
-    #j = day
-    
-    for i in range(1,len(lists)):
+        self.username = username
+        self.password = password
+
+        self.driver = webdriver.Firefox()
+
+        self.driver.get('https://my.sdu.edu.kz/')
+
+    def login(self):
+
         
-        tds = lists[i].find_all('td')
+        self.driver.find_element_by_id("username").send_keys(self.username)
+        self.driver.find_element_by_id("password").send_keys(self.password)
+        self.driver.find_element_by_class_name("q-button").click()
 
-        for j in range(1,len(tds)):
-            
-            span = tds[j].find('span')
+    def get_to_schedule(self):
 
-            if(len(span) == 1):
-                continue
+        self.login()
+        
+        self.driver.find_element_by_css_selector(".leftLinks a[href^='?mod=schedule'] ").click()
+        sleep(2)
+        html = self.driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+        return html
+        
 
+    def get_schedule_data(self):
+        
+        html = self.get_to_schedule()
+        soup = BeautifulSoup(html,'lxml')
+        
+        lists = soup.find('div',id='div_results').find_all('tr')
+        weekdays = {1:[],
+                    2:[],
+                    3:[],
+                    4:[],
+                    5:[],
+                    6:[]}
+
+        #i = time
+        #j = day
+        
+        for i in range(1,len(lists)):
             
-            title = span.find('a')["title"]
-            
-            imgs = span.find_all('img')
-            teacher_name = imgs[0]["title"]
-            
-            room = imgs[1]["title"]
-            
-            time = tds[0].find('span').text
-            subject = Subject(title,teacher_name,room,j,time)
-            
-            subjects.append(subject)
-            
-    subjects = sorted(subjects)
-    for l in range(len(subjects)):
-        print(subjects[l])
+            tds = lists[i].find_all('td')
+
+            for j in range(1,len(tds)):
+                
+                span = tds[j].find('span')
+
+                if(len(span) == 1):
+                    continue
+
+                
+                title = span.find('a')["title"]
+                
+                imgs = span.find_all('img')
+                teacher_name = imgs[0]["title"]
+                
+                room = imgs[1]["title"]
+                
+                time = tds[0].find('span').text
+                subject = Subject(title,teacher_name,room,time)
+
+                weekdays[j].append(subject) 
+
+        return weekdays               
+                
+    
     
 
-get_schedule_data()
+
 
