cf00ceac08d30b862d4cb6632f62b810db38a9e1
YerlanTemir <170103024@stu.sdu.edu.kz>
2019-03-02 13:25:37 +0600

    refactor(bot.py):change the way of asking user info

--- bot.py
+++ bot.py
@@ -13,9 +13,108 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 username = ''
 password = ''
 
-USERNAME,PASSWORD,GENDER = range(3)
 
 weekdays = {}
+
+
+
+
+
+
+
+def start(bot,update):
+    
+    update.message.reply_text('Welcome to our bot!')
+
+
+def set_username(bot,update,args):
+
+    global username
+    
+    try:
+    
+        username = args[0]
+    
+    except IndexError:
+
+        update.message.reply_text('Your foggot to enter your login(example: /set_username 170103024),please,try again')
+        return
+    
+    update.message.reply_text('Username saved')
+
+
+def set_password(bot,update,args):
+    
+    global password
+    try:
+        password = args[0]
+    except IndexError:
+
+        update.message.reply_text('Your foggot to enter your password(example: /set_password 170103024),please,try again')
+        return
+    
+    update.message.reply_text('Password saved')
+    
+
+def save_schedule(bot,update):
+    
+    global username,password,weekdays
+    
+    if(username == '' or password == ''):
+
+        update.message.reply_text('You did not entered your login or password,please,firtsly use /set_username and /set_password methods')
+        return
+
+    update.message.reply_text('We are getting data from portal,wait about 10 seconds')
+    
+    try:
+        schedule = scrapper.Schedule(username,password)
+    
+    except NoSuchElementException:
+
+        update.message.reply_text('Your entered incorrect username/password,so we could not get your schedule,try again!')
+        return
+    
+    weekdays = schedule.get_schedule_data()
+    update.message.reply_text('We got your schedule,you are welcome to use!')
+
+
+def get_schedule(bot,update,args):
+    
+    try:
+        
+        day = int(args[0])
+
+    except IndexError:
+        
+        update.message.reply_text('Your foggot to enter day(example: /get_schedule 2 ,to get tuesday\'s schedule),please,try again')
+        return
+    
+    global weekdays
+
+    for i in range(len(weekdays[day])):
+        update.message.reply_text('{}){}'.format(i+1,weekdays[day][i]))
+
+
+
+
+def main():
+
+    TOKEN = sd.get_token()
+
+    updater = Updater(token = TOKEN)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start',start))
+    dp.add_handler(CommandHandler('set_username',set_username,pass_args=True))
+    dp.add_handler(CommandHandler('set_password',set_password,pass_args=True))
+    dp.add_handler(CommandHandler('save_schedule',save_schedule))
+    dp.add_handler(CommandHandler('get_schedule',get_schedule,pass_args=True))
+    dp.add_handler(CommandHandler('next',callback_get_time))
+    
+    updater.start_polling()
+
+
 
 
 def callback_get_time(bot,update):
@@ -23,10 +122,12 @@ def callback_get_time(bot,update):
     date = update.message.date
     time = datetime.time(date.hour,date.minute,date.second)
     weekday = date.isoweekday()
+    
     days = 0
     
-    if(weekday >= 6):
-        days = 8-weekday
+    if(weekday == 7):
+        
+        days = 1
         weekday = 1
 
     indexOfMin = closest_index(time,weekday)
@@ -49,6 +150,7 @@ def callback_get_time(bot,update):
     
 
 def closest_index(time,weekday):
+
     my_time_minutes = time.hour*60 + time.minute
     min = 24*60
     indexOfMin = 0
@@ -61,86 +163,12 @@ def closest_index(time,weekday):
         difference = minutes - my_time_minutes 
 
         if(difference < min and difference > 0):
+
             min = difference
             indexOfMin = i
         
 
     return indexOfMin
-
-
-
-
-def start(bot,update):
-    update.message.reply_text('Enter your login:')
-
-    return USERNAME
-    
-
-
-def get_username(bot,update):
-    
-    global username
-    username = update.message.text
-    update.message.reply_text('Now,enter your password:')
-    
-    return PASSWORD
-
-
-def get_password(bot,update):
-
-    global password
-    password = update.message.text
-    update.message.reply_text('Wait about 10 seconds,we are getting data from portal')
-
-    try:
-    
-        schedule = scrapper.Schedule(username,password)
-
-    except NoSuchElementException:
-        print('kek')
-        update.message.reply_text('Your entered incorrect data,try again:')
-        update.message.reply_text('Enter your login:')
-        
-        return USERNAME
-
-    global weekdays
-    weekdays = schedule.get_schedule_data()
-
-    update.message.reply_text('Your are welcome,now you can use our bot!')
-    
-    
-    return 
-
-def cancel(bot,update):
-    
-    return ConversationHandler.END
-
-
-def main():
-
-    TOKEN = sd.get_token()
-
-    updater = Updater(token = TOKEN)
-    dp = updater.dispatcher
-    
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
-
-        states={
-            
-            USERNAME: [MessageHandler(Filters.text, get_username)],
-
-            PASSWORD: [MessageHandler(Filters.text, get_password)]
-
-        },
-
-        fallbacks=[CommandHandler('cancel', cancel)]
-    )
-   
-
-    dp.add_handler(CommandHandler('next',callback_get_time))
-    dp.add_handler(conv_handler)    
-    updater.start_polling()
 
 
 if __name__ == '__main__':
