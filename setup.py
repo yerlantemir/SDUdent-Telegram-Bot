2ab4c733e9c466c0fa51943012632d53b7ab29fe
YerlanTemir <170103024@stu.sdu.edu.kz>
2019-04-10 23:44:24 +0600

    perf(setup.py):decrease time interval

--- setup.py
+++ setup.py
@@ -139,7 +139,7 @@ def notify_grades(bot_):
             notify_about_new_grade(bot_,appends,updates,new_grades,old_grades,chat_id)
             db.set_data(["users",chat_id,"grades_data"],new_grades)
 
-        time.sleep(7200)
+        time.sleep(600)
 
 
 def notify_about_new_grade(bot_,appends,updates,new_grades,old_grades,chat_id):
